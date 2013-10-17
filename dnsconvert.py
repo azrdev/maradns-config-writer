8046bb543c216c3a1603ae5c979956e6f94e62f5
jBiegert <azrdev@qrdn.de>
2013-10-17 02:20:11 +0200

    Python 3.2 compatibility, detailed error output

--- dnsconvert.py
+++ dnsconvert.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 import sys
 import argparse
-import ipaddress
 import re
+try:
+    import ipaddress
+except:
+    # for python < 3.3, install ipaddr from your distro or http://code.google.com/p/ipaddr-py/
+    import ipaddr as ipaddress
+
 
 # network specific configuration
 
@@ -28,14 +33,14 @@ enabled. Lines starting with # or containing only whitespace are ignored. Every
 other line needs 3 columns:
  - An IPv4 address
  - An IPv6 address
- - A list of hostnames, separated by ','
+ - A list of lower-case hostnames, separated by ','
 One of the addresses may be replaced by 'none'.
 
 The output configuration files contain record types A, AAAA and PTR. For PTR
 records, the first hostname in the list for the respective address is used.
 
-The input file is only accepted if it is valid and addresses belong into the 
-networks, else if the input gets rejected, no output file is touched: 
+The input file is only accepted if it is valid syntax and addresses belong into the
+following networks, else if the input gets rejected, no output file is touched:
   {ip4_range} or
   {ip6_range}""".format(ip4_range=ip4_range, ip6_range=ip6_range))
 
@@ -66,12 +71,12 @@ def handleLine(ip4, ip6, names, outp, generate):
 
     # at least one address has to be supplied
     if ip4 is None and ip6 is None:
-        print("Neither IPv4 nor IPv6 address were specified.", file=debugOut)
+        print("Neither IPv4 nor IPv6 address were specified for name(s) " + ','.join(names), file=debugOut)
         return False
 
-    # there must be at least one name
+    # there must be at least one name (currently this is handled by the line-parser, so this should never happen)
     if len(names) <= 0:
-        print("No names were specified.", file=debugOut)
+        print("No names were specified for {}, {}".format(ip4, ip6), file=debugOut)
         return False
 
     try:
@@ -80,7 +85,7 @@ def handleLine(ip4, ip6, names, outp, generate):
         else:
             ip4addr = ipaddress.IPv4Address(ip4)
             if not ip4addr in ip4_range:
-                print("IPv4 address not in permitted range", file=debugOut)
+                print("IPv4 address not in permitted range " + str(ip4_range), file=debugOut)
                 return False
 
         if ip6 is None:
@@ -88,16 +93,16 @@ def handleLine(ip4, ip6, names, outp, generate):
         else:
             ip6addr = ipaddress.IPv6Address(ip6)
             if not ip6addr in ip6_range:
-                print("IPv6 address not in permitted range", file=debugOut)
+                print("IPv6 address not in permitted range " + str(ip6_range), file=debugOut)
                 return False
 
-    except:
-        print("Invalid IP address", file=debugOut)
+    except Exception as e:
+        print("Invalid IP address: " + repr(e), file=debugOut)
         return False
 
     for name in names:
         if nameRegex.match(name) is None:
-            print("Name '{}' invalid".format(name), file=debugOut)
+            print("Invalid name \"{}\", does not match {}".format(name, nameRegex.pattern), file=debugOut)
             return False
 
     #TODO: check for double addresses
@@ -142,19 +147,20 @@ counter = Counter()
 
 generate = not args.check_only
 
-for rawRow in inputfile:
+for rawLineNo, rawRow in enumerate(inputfile, 1):
     # comment
     if rawRow.startswith('#'):
         continue
 
-    row = rawRow.split(maxsplit=3)
+    row = rawRow.split()
 
     # empty line
     if len(row) <= 0:
         continue
 
     # missing column(s)
-    if len(row) < 3:
+    if len(row) != 3:
+        print("Ill-formed line {}: {} colums, expected 3".format(rawLineNo, len(row)), file=debugOut)
         sys.exit(1)
 
     ip4 = row[0]
@@ -167,13 +173,19 @@ for rawRow in inputfile:
         ip6 = None
 
     if not handleLine(ip4, ip6, names, outp=outConfig, generate=generate):
+        print("Error occured on line {}, aborting.".format(rawLineNo), file=debugOut)
         sys.exit(1)
 
     counter.add(ip4, ip6, len(names))
 
+inputfile.close()
+
 if generate:
     #print("## A and AAAA config\n{}\n## IPv4 PTRs\n{}\n## IPv6 PTRs\n{}\n".format(outConfig['arecords'], outConfig['aptrs'], outConfig['aaaaptrs']), file=debugOut)
-    print("Generation successful.\n   A records: {noA}\nAAAA records: {noAAAA}\n PTR records: {noPTR}\n".format(noA=counter.A, noAAAA=counter.AAAA, noPTR=counter.PTR))
+    print("""Generation successful.
+   A records: {noA}
+AAAA records: {noAAAA}
+ PTR records: {noPTR}\n""".format(noA=counter.A, noAAAA=counter.AAAA, noPTR=counter.PTR))
 
     for conf in outConfig:
         outFile = open(args.outputprefix + outPathSuffix[conf], 'w' if args.override_output else 'a')
